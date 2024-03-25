2f97107b8b75b7d03c11817c61d94915cc430a25
Rhyan Michelin <53277086+rhyanmichelin@users.noreply.github.com>
2024-03-25 19:05:07 -0300

    Add files via upload

---
+++ portfolio/menu.js
@@ -0,0 +1,32 @@
+
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+// DARK MODE E LIGHT MODE //
+
+let trilho = document.getElementById('trilho')
+let body = document.querySelector('body')
+
+trilho.addEventListener('click', ()=>{
+    trilho.classList.toggle('dark')
+    body.classList.toggle('dark')
+})
+
+// teste mudar imagem //
+
+
+
+
